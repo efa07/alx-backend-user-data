52f4c9a6f540a1ad074d3037107d66b5df735276
efa07 <efatariku07@gmail.com>
2024-08-06 14:02:52 +0300

    type fixed in line 26

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,7 +4,6 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -12,7 +11,6 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -25,8 +23,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """Endpoint that triggers a 401 Unauthorized error"""
     abort(401)
