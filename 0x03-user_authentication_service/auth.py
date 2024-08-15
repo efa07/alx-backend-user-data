383080a61e292850950bee9bd97b2341854b9e38
efa07 <efatariku07@gmail.com>
2024-08-15 18:29:42 +0300

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,14 +15,6 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    @staticmethod
-    def _hash_password(password: str) -> bytes:
-        """Hashes a password using bcrypt."""
-        import bcrypt
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
-
     def register_user(self, email: str, password: str) -> User:
         """Register a new user with hashed password.
 
@@ -60,6 +52,113 @@ class Auth:
             return False
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session and returns the session ID as a string.
+
+        Args:
+            email (str): Email of user to create session for.
+
+        Returns:
+            str: Session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieve a User object from a session ID.
+
+        Args:
+            session_id (str): The ID of the session to retrieve the user from.
+
+        Returns:
+            Union[User, None]: A User object corresponding to the session ID if
+            one exists, otherwise None.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Method to destroy the session associated with a user
+
+        Args:
+            user_id (int): The ID of the user whose session is to be destroyed.
+
+        Returns:
+            None
+        """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a password reset token for a user.
+
+        Args:
+            email (str): A string representing the email address of the user to
+            generate a password reset token for.
+
+        Raises:
+            ValueError: If no user with the specified email address is found.
+
+        Returns:
+            str: A string representing the password reset token generated for
+            the user.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password using a reset token.
+
+        Args:
+            reset_token (str): The reset token associated with the user.
+            password (str): The new password to set.
+
+        Raises:
+            ValueError: If the reset token is invalid (i.e., not associated
+            with a user)..
+
+        Returns:
+            None
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
+        new_hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_hashed_password,
+            reset_token=None,
+        )
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    import bcrypt
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
 
 
 def _generate_uuid() -> str:
