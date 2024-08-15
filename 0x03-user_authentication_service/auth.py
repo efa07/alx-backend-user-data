e59479ea4170542eead7f9995d1d36cc1de968b5
efa07 <efatariku07@gmail.com>
2024-08-15 18:31:24 +0300

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,7 +72,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Retrieve a User object from a session ID.
 
         Args:
