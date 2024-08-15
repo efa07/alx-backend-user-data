c9a9993706cd88f067408819556fcc5081be1250
efa07 <efatariku07@gmail.com>
2024-08-15 16:08:19 +0300

    class added

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,43 @@
-#!/usr/bin/env python3
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
-import bcrypt
 
+class Auth:
+    """Auth class to interact with the authentication database."""
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password using bcrypt."""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    def __init__(self):
+        self._db = DB()
+
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """Hashes a password using bcrypt."""
+        import bcrypt
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with hashed password.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The newly created User object.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email=email,
+                                         hashed_password=hashed_password)
+            return new_user
+        except InvalidRequestError as e:
+            raise ValueError(f"Invalid request: {e}")
