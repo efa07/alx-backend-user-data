7894aa6b8727f79629068aaa527c062d93331107
efa07 <efatariku07@gmail.com>
2024-08-15 16:02:58 +0300

    change it function mode from class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,21 +3,8 @@
 import bcrypt
 
 
-class Auth:
-    """Auth class to handle password hashing"""
-
-    @staticmethod
-    def _hash_password(password: str) -> bytes:
-        """Hashes a password using bcrypt.
-
-        Args:
-            password: The password to hash.
-
-        Returns:
-            bytes: The salted hash of the password.
-        """
-        salt = bcrypt.gensalt()
-
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-        return hashed_password
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
